2cc34689f1cf5832112c2401995a26e31435994c
Matias Porolli <31012243+pr0li@users.noreply.github.com>
2017-08-26 03:11:30 -0300

    Update ner.sh

--- stanford-ner-2016-10-31/ner.sh
+++ stanford-ner-2016-10-31/ner.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 scriptdir=`dirname $0`
 
-java -mx2g -cp "$scriptdir/stanford-ner.jar:$scriptdir/lib/*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier $scriptdir/classifiers/spanish.ancora.distsim.s512.crf.ser.gz -textFile $1
+java -mx1g -cp "$scriptdir/stanford-ner.jar:$scriptdir/lib/*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier $scriptdir/classifiers/spanish.ancora.distsim.s512.crf.ser.gz -textFile $1
