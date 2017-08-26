6d74530765104f0a03acd263b6a7010ad59c5c41
Matias Porolli <31012243+pr0li@users.noreply.github.com>
2017-08-26 03:11:06 -0300

    Update stanford-postagger.sh

--- stanford-postagger-full-2016-10-31/stanford-postagger.sh
+++ stanford-postagger-full-2016-10-31/stanford-postagger.sh
@@ -3,4 +3,4 @@
 # usage: ./stanford-postagger.sh model textFile
 #  e.g., ./stanford-postagger.sh models/english-left3words-distsim.tagger sample-input.txt
 
-java -mx2g -cp 'stanford-postagger.jar:' edu.stanford.nlp.tagger.maxent.MaxentTagger -model models/spanish-distsim.tagger -textFile $1
+java -mx1g -cp 'stanford-postagger.jar:' edu.stanford.nlp.tagger.maxent.MaxentTagger -model models/spanish-distsim.tagger -textFile $1
